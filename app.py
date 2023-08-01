e7f6163c8a1264214382587cc640f0a2c31e68cb
Gabriel-Angelo-Silva <136750547+Gabriel-Angelo-Silva@users.noreply.github.com>
2023-08-01 12:32:08 -0400

    app.py

--- app.py
+++ app.py
@@ -9,18 +9,7 @@ app.config['NOME_SITE'] = 'Contador de Tags HTML'
 
 @app.route('/')
 def indice():
-    return render_template('indice.html', nome_site=app.config['NOME_SITE'])
-
-@app.route('/contar_tags', methods=['POST'])
-def contador_tag():
-    entrada_dados = request.form['entrada_dados']
-    
-    try:
-        codigo = obter_codigo(entrada_dados)
-        contador_de_tags = contar_tags(codigo)
-        return render_template('resultado.html', contador_de_tags = contador_de_tags)
-    except requests.exceptions.RequestException as e:
-        return "<p>Erro ao acessar a URL ou analisar o código HTML.</p>"
+    return render_template('indice.html', nome_site = app.config['NOME_SITE'])
 
 def obter_codigo(entrada_dados):
     if entrada_dados.startswith(("http://", "https://")):
@@ -28,10 +17,16 @@ def obter_codigo(entrada_dados):
         resposta.raise_for_status()
         return resposta.text
     return entrada_dados
-
-def contar_tags(codigo):
-    soup = BeautifulSoup(codigo, 'html.parser')
-    contador_de_tags = {}
-    for tag in soup.find_all():
-        contador_de_tags[tag.name] = contador_de_tags.get(tag.name, 0) + 1
-    return contador_de_tags
+    
+@app.route('/contar_tags', methods = ['POST'])
+def contar_tags():
+    entrada_dados = request.form['entrada_dados']
+    try:
+        codigo = obter_codigo(entrada_dados)
+        contador_de_tags = {}
+        soup = BeautifulSoup(codigo, 'html.parser')
+        for tag in soup.find_all():
+            contador_de_tags[tag.name] = contador_de_tags.get(tag.name, 0) + 1
+        return render_template('resultado.html', contador_de_tags = contador_de_tags)
+    except requests.exceptions.RequestException as e:
+        return "<p>Erro ao acessar a URL ou analisar o código HTML.</p>"
